e5351f26c8f1d9750bf0f14344538e061eb99efb
unknown <e-jiliang.li@lotuscars.com.cn>
2024-05-11 16:12:35 +0800

    feat: 局部变量， 全局变量， 形参区分

--- src/fun/funTest.go
+++ src/fun/funTest.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 )
 
+const WEEK = "MONDAY" // 全局变量
+var testStr string
+var globalStr string = "globalStr out of method"
+
 func main() {
 	fmt.Println("Hello World")
-	num1 := 100
+	num1 := 100 // 局部变量
 	num2 := 200
 	maxNum := maxInt(num1, num2)
 	fmt.Println(maxNum)
 
 	str1, str2 := swapVal("hello", "world")
 	fmt.Println(str1, str2)
+	fmt.Println(WEEK)
+	testStr = "Global val"
+	var globalStr string = "hello global str in method"
+	fmt.Println(testStr)
+	fmt.Println(globalStr)
 }
 
-func maxInt(num1 int, num2 int) int {
+func maxInt(num1 int, num2 int) int { //num1 num2 形参
 	if num1 > num2 {
 		return num1
 	}
