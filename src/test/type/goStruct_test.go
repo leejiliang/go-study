dc51a86b6e24cb3a8516ec9e713196f041bfa429
jiliang.li <leejiliang@126.com>
2024-05-21 18:00:15 +0800

    feat: 测试自定义类型

--- src/test/type/goStruct_test.go
+++ src/test/type/goStruct_test.go
@@ -1,6 +1,10 @@
 package _type
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 type Book struct {
 	title   string
@@ -28,4 +32,25 @@ func getTitle(book Book) string {
 
 func getAuthor(book *Book) string {
 	return book.author
+}
+
+type MyFun func(v int) int
+
+func InjectTest(inMe MyFun) MyFun {
+	return func(v int) int {
+		start := time.Now()
+		res := inMe(v)
+		fmt.Println("time spent ", time.Since(start).Seconds())
+		return res
+	}
+}
+
+func SlowFn(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestInject(t *testing.T) {
+	test := InjectTest(SlowFn)
+	t.Log(test(10))
 }
