c86b3bc4fd886dfce19d9c0ca74ca34abceafd43
jiliang.li <leejiliang@126.com>
2024-05-22 11:21:07 +0800

    feat: use sync.mutex 解决并发安全问题, sync.WaitGroup 解决线程等待问题

--- src/goroutine/goroutine_test.go
+++ src/goroutine/goroutine_test.go
@@ -2,7 +2,9 @@ package goroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestGoroutine(t *testing.T) {
@@ -11,4 +13,50 @@ func TestGoroutine(t *testing.T) {
 			fmt.Printf("result is %d \n", j)
 		}(i)
 	}
+}
+
+func TestGoroutineUnsafe(t *testing.T) {
+	var sum = 0
+	for i := 0; i < 1000; i++ {
+		go func() { // 开启协程完成任务
+			sum++
+		}()
+	}
+	time.Sleep(time.Second * 3)
+	t.Logf("sum is : %d", sum)
+}
+
+func TestGoroutineSafe(t *testing.T) {
+	var mux sync.Mutex
+	var sum = 0
+	for i := 0; i < 1000; i++ {
+		go func() { // 开启协程完成任务
+			defer func() {
+				mux.Unlock() //释放锁
+			}()
+			mux.Lock() // 加锁
+			sum++
+		}()
+	}
+	time.Sleep(time.Second * 3)
+	t.Logf("sum is : %d", sum)
+}
+
+func TestGoroutineWait(t *testing.T) {
+	var mux sync.Mutex
+	var wg sync.WaitGroup
+	var sum = 0
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() { // 开启协程完成任务
+			defer func() {
+				mux.Unlock() //释放锁
+			}()
+			mux.Lock() // 加锁
+			sum++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("sum is : %d", sum)
 }
