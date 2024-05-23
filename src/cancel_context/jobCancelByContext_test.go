79741ffad279b32a46c5486eff80d754cdb329d2
jiliang.li <leejiliang@126.com>
2024-05-23 10:38:52 +0800

    feat: 利用context 取消任务

--- src/cancel_context/jobCancelByContext_test.go
+++ src/cancel_context/jobCancelByContext_test.go
@@ -1,6 +1,7 @@
-package cancel
+package cancel_context
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
@@ -16,24 +17,25 @@ func produceData(ch chan int) {
 	}()
 }
 
-func isChanCancel(cancelCh chan struct{}) bool {
+func isChanCancel(ctx context.Context) bool {
 	select {
-	case <-cancelCh:
+	case <-ctx.Done():
+
 		return true
 	default:
 		return false
 	}
 }
 
-func receiveData(ch chan int, cancelCh chan struct{}, group *sync.WaitGroup) {
+func receiveData(ch chan int, ctx context.Context, group *sync.WaitGroup) {
 	for tt := 0; tt < 5; tt++ {
 		group.Add(1)
 		go func() {
 			for {
-				if !isChanCancel(cancelCh) {
+				if !isChanCancel(ctx) {
 					if i, open2 := <-ch; open2 {
 						fmt.Printf("receive data : %d \n", i)
-						time.Sleep(time.Millisecond * 2000)
+						time.Sleep(time.Millisecond * 1000)
 					} else {
 						fmt.Println("producer closed channel")
 						break
@@ -51,11 +53,11 @@ func receiveData(ch chan int, cancelCh chan struct{}, group *sync.WaitGroup) {
 
 func TestCancelJob(t *testing.T) {
 	var dataCh = make(chan int, 10)
-	var cancelCh = make(chan struct{}, 1)
+	var ctx, cancel = context.WithCancel(context.Background())
 	var wg sync.WaitGroup
 	produceData(dataCh)
-	receiveData(dataCh, cancelCh, &wg)
-	time.Sleep(time.Second * 1)
-	close(cancelCh)
+	receiveData(dataCh, ctx, &wg)
+	time.Sleep(time.Second * 3)
+	cancel()
 	wg.Wait()
 }
