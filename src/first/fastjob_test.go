ed3277935905e8eafeaabbefa58a406c07d63914
jiliang.li <leejiliang@126.com>
2024-05-24 12:13:48 +0800

    feat: 测试使用waitGroup和channel 实现获取全部任务的执行结果

--- src/first/fastjob_test.go
+++ src/first/fastjob_test.go
@@ -26,3 +26,25 @@ func TestGetFirstJobResult(t *testing.T) {
 	time.Sleep(time.Second * 1)
 	t.Logf("after runtime goroutine: %d", runtime.NumGoroutine())
 }
+
+func TestGetAllJobResult(t *testing.T) {
+	//var wg sync.WaitGroup
+	t.Logf("beautiful runtime goroutine: %d", runtime.NumGoroutine())
+	var ch = make(chan string, 10)
+	for i := 0; i < 10; i++ {
+		//wg.Add(1)
+		go func(val int) {
+			ret := doSomething(val)
+			ch <- ret
+			//wg.Done()
+		}(i)
+	}
+	//wg.Wait()
+	//close(ch)
+	for i := 0; i < 10; i++ {
+		ret := <-ch
+		t.Log(ret)
+	}
+	time.Sleep(time.Second * 1)
+	t.Logf("after runtime goroutine: %d", runtime.NumGoroutine())
+}
