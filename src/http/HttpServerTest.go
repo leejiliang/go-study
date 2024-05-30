599c824da25dcb5d8070b214251cc83c0aedae46
jiliang.li <leejiliang@126.com>
2024-05-30 10:06:42 +0800

    feat: 使用go语言内置的http提供简单的http服务

---
+++ src/http/HttpServerTest.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World"))
+		if err != nil {
+			return
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+
+}
